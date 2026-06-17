9d087bbf956af9026a8d920d687a699b0349d47a
GabiMinecraft02 <gabrielmeresse09@gmail.com>
2026-06-17 17:23:15 +0200

    fix

--- WebSite_Manager.py
+++ WebSite_Manager.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+base_dir = os.path.abspath(os.path.dirname(__file__))
+template_dir = os.path.join(base_dir, 'templates')
+
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def index():
